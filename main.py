dcea44c8670968231359b4256ccdaa3c85b9801e
Morning Dove Nelson <morningdhansen@gmail.com>
2024-04-23 13:36:27 -0600

    All get/post/put/delete work

--- main.py
+++ main.py
@@ -1,12 +1,65 @@
-"""
-Have these endpoints:
+from fastapi import FastAPI
 
-GET / -> list[airline_name]
-GET /:airline_name -> list[flight_num]
-GET /:airline_name/:flight_num -> Flight
+import json
+from models import Flight, AirlineName
 
-POST /:airline
-PUT /:airline/:flight_num
-DELETE /:airline/:flight_num
 
-"""
+app = FastAPI()
+
+with open("airlines.json", "r") as f:
+    airlines: dict = json.load(f)
+
+flights: dict[AirlineName, list[Flight]] = {}
+
+for airline_name, flight_list in airlines.items():
+    f_list: list[Flight] = []
+    for flight in flight_list:
+        f_list.append(Flight(**flight))
+    flights[AirlineName(airline_name)] = f_list
+
+
+# GET / -> list[airline_name]
+@app.get("/")
+async def get_airline_list() -> list[AirlineName]:
+    return flights.keys()
+
+
+# GET /:airline_name -> list[flight_num]
+@app.get("/{airline_name}")
+async def get_airline_flight(airline_name: AirlineName) -> list[str]:
+    airline_flights = flights.get(airline_name, [])
+    return [flight.flight_num for flight in airline_flights]
+
+
+# GET /:airline_name/:flight_num -> Flight 
+@app.get("/{airline_name}/{flight_num}")
+async def get_flight(airline_name: AirlineName, flight_num: str) -> Flight:
+    for flight in flights[AirlineName(airline_name)]:
+        if flight.flight_num == flight_num:
+            return flight
+
+
+# POST /:airline 
+@app.post("/{airline}")
+async def create_flight(airline_name: AirlineName, flight: Flight) -> None:
+    flights[airline_name].append(flight)
+
+
+# PUT /:airline/:flight_num
+@app.put("/{airline}/{flight_num}")
+async def update_flight(airline_name: AirlineName, flight_num: str, updated_flight: Flight) -> None:
+    for flight in flights[AirlineName(airline_name)]:
+        if flight.flight_num == flight_num:
+            flight.capacity = updated_flight.capacity
+            flight.estimated_flight_duration = updated_flight.estimated_flight_duration
+    
+
+# DELETE /:airline/:flight_num
+@app.delete("/{airline}/{flight_num}")
+async def delete_flight(airline_name: AirlineName, flight_num: str) -> str:
+    airline_flights = flights.get(airline_name, [])
+    for i, flight in enumerate(airline_flights):
+        if flight.flight_num == flight_num:
+            airline_flights.pop(i)
+            return "Flight deleted"
+
---
+++ models.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from enum import Enum
+
+
+class AirlineName(Enum):
+    DELTA = "Delta"
+    SOUTHWEST = "Southwest"
+    ALASKA = "Alaska"
+
+
+class Flight(BaseModel):
+    flight_num: str
+    capacity: int
+    estimated_flight_duration: int
+
+
+class Airline(BaseModel):
+    flights: list[Flight]
